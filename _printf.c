4371becac3dd2eb4cb7cc335e3eed26ed36b0c17
AngelJCT <acarrion_pr@yahoo.com>
2022-11-08 20:43:32 +0000

    fixing returning count in main file

--- _printf.c
+++ _printf.c
@@ -7,12 +7,10 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0, i = 0;
+	int cnt = 0, i = 0;
 	va_list ptr;
 
 	va_start(ptr, format);
-	while (format && format[count])
-		count++;
 	while (format[i])
 	{
 		if (format[i] == '%')
@@ -21,27 +19,26 @@ int _printf(const char *format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					get_char(va_arg(ptr, int));
+					cnt += get_char(va_arg(ptr, int));
 					break;
 				case 's':
-					get_string(va_arg(ptr, char *));
+					cnt += get_string(va_arg(ptr, char *));
 					break;
 				case '%':
 					write(1, &format[i], 1);
+					cnt++;
 					break;
 				case ' ': case '\0':
 					return (-1);
-				default:
-					i++;
-					continue;
 			}
 		}
 		else
 		{
 			write(1, &format[i], 1);
+			cnt++;
 		}
 		i++;
 	}
 	va_end(ptr);
-	return (count);
+	return (cnt);
 }
